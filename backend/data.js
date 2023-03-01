b7527bae3a85d5f954ad5f6b5a7c5945ebfd5393
Rahul paul Chowdhury <78229611+Starlight404@users.noreply.github.com>
2023-03-02 03:00:12 +0600

    installing axios

---
+++ backend/data.js
@@ -0,0 +1,81 @@
+module.exports =  {
+    products: [
+        {
+            _id:'1',
+            name: 'Leather Shoes',
+            category:'Men',
+            image:'/images/product-1.png',
+            price:12035,
+            brand:'Shoemate',
+            rating:4.5,
+            numReviews:10,
+            countInStock:5
+        },
+        {
+            _id:'2',
+            name: 'Leather Shoes',
+            category:'Men',
+            image:'/images/product-2.png',
+            price:1850,
+            brand:'Shoemate',
+            rating:4.5,
+            numReviews:10,
+            countInStock:5
+        },
+        {
+            _id:'3',
+            name: 'Leather Shoes',
+            category:'Men',
+            image:'/images/product-2.png',
+            price:1850,
+            brand:'Shoemate',
+            rating:4.5,
+            numReviews:10,
+            countInStock:5
+        },
+        {
+            _id:'4',
+            name: 'Leather Shoes',
+            category:'Men',
+            image:'/images/product-1.png',
+            price:1850,
+            brand:'Shoemate',
+            rating:4.5,
+            numReviews:10,
+            countInStock:5
+        },
+        {
+            _id:'5',
+            name: 'Leather Shoes',
+            category:'Men',
+            image:'/images/product-1.png',
+            price:1850,
+            brand:'Shoemate',
+            rating:4.5,
+            numReviews:10,
+            countInStock:5
+        },
+        {
+            _id:'6',
+            name: 'Leather Shoes',
+            category:'Men',
+            image:'/images/product-1.png',
+            price:1850,
+            brand:'Shoemate',
+            rating:4.5,
+            numReviews:10,
+            countInStock:5
+        },
+        {
+            _id:'7',
+            name: 'Leather Shoes',
+            category:'Men',
+            image:'/images/product-1.png',
+            price:1850,
+            brand:'Shoemate',
+            rating:4.5,
+            numReviews:10,
+            countInStock:5
+        },
+    ],
+};
