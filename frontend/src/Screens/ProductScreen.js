c53c7c0db1cc055f760bd9f9969b79b11d8959d1
Rahul paul Chowdhury <78229611+Starlight404@users.noreply.github.com>
2023-03-06 17:32:53 +0600

    created productscreen from backend

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,7 +1,25 @@
+import { getProduct } from "../api";
+import { parseRequestUrl } from "../utils";
+
 const ProductScreen = {
-  render: () => {
-    return `<div>ProductScreen</div>`;
+  render: async () => {
+    const request = parseRequestUrl();
+    const { id } = request;
+    const product = await getProduct(id);
+    
+    if (product.error) {
+      return `<div>Error: ${product.error}</div>`;
+    }
+
+    return `
+      <div class="loading">Loading...</div>
+      <h1>${product.name}</h1>
+    `;
   },
 };
 
 export default ProductScreen;
+
+
+
+
