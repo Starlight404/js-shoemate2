366ce7f01d267dc6eaa2491ccf3dfc59dd781679
Rahul paul Chowdhury <78229611+Starlight404@users.noreply.github.com>
2023-03-08 23:12:07 +0600

    wip:connected to mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,37 @@
 import express from "express";
 import cors from "cors";
 import data from "./data";
+import mongoose from "mongoose";
+import config from "./config";
+
+
+mongoose
+  .connect(config.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
 
 const app = express();
+
 app.use(cors());
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id.toString() === req.params.id.toString());
+  const product = data.products.find(
+    (x) => x._id.toString() === req.params.id.toString()
+  );
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({message: 'Product has not been found'});
+    res.status(404).send({ message: "Product has not been found" });
   }
 });
 
--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -23,31 +23,31 @@ const addToCart = (item, forceUpdate = false) => {
 
 const removeFromCart = (id) => {
   setCartItems(getCartItems().filter((x) => x.product !== id));
-  if(id=== parseRequestUrl().id){
-    document.location.hash  = '/cart';
-  }else{
+  if (id === parseRequestUrl().id) {
+    document.location.hash = "/cart";
+  } else {
     rerender(CartScreen);
   }
-}
+};
 
 const CartScreen = {
   after_render: () => {
     const qtySelects = document.getElementsByClassName("qty-select");
-    Array.from(qtySelects).forEach(qtySelect => {
+    Array.from(qtySelects).forEach((qtySelect) => {
       qtySelect.addEventListener("change", (e) => {
-        const item = getCartItems().find( x => x.product === qtySelect.id);
+        const item = getCartItems().find((x) => x.product === qtySelect.id);
         addToCart({ ...item, qty: Number(e.target.value) }, true);
       });
     });
     const deleteButtons = document.getElementsByClassName("delete-button");
-    Array.from(deleteButtons).forEach(deleteButton => {
-      deleteButton.addEventListener('click',() => {
+    Array.from(deleteButtons).forEach((deleteButton) => {
+      deleteButton.addEventListener("click", () => {
         removeFromCart(deleteButton.id);
-      })
+      });
     });
-    document.getElementById("checkout-button").addEventListener('click',() => {
-      document.location.hash = '/signin';
-    })
+    document.getElementById("checkout-button").addEventListener("click", () => {
+      document.location.hash = "/signin";
+    });
   },
   render: async () => {
     const request = parseRequestUrl();
@@ -88,11 +88,10 @@ const CartScreen = {
             <div>
               Qty:
               <select class="qty-select" id="${item.product}">
-              ${
-                [...Array(item.countInStock).keys()].map(x =>
+              ${[...Array(item.countInStock).keys()].map((x) =>
                 item.qty === x + 1
-                  ? `<option selected value = "${x+1}">${x + 1}</option>`
-                  : `<option  value = "${x+1}">${x + 1}</option>`
+                  ? `<option selected value = "${x + 1}">${x + 1}</option>`
+                  : `<option  value = "${x + 1}">${x + 1}</option>`
               )}  
               </select>
               <button type="button" class="delete-button" id="${item.product}">
