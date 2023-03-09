417d544d4154efe71298bf3e24b274f8879adb15
Rahul paul Chowdhury <78229611+Starlight404@users.noreply.github.com>
2023-03-09 15:40:34 +0600

    wip: signIn page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,14 @@ import ProductScreen from "./Screens/ProductScreen.js";
 import Error404Screen from "./Screens/Error404Screen.js";
 import { parseRequestUrl } from "./utils.js";
 import CartScreen from "./Screens/CartScreen.js";
+import SigninScreen from "./Screens/SigninScreen.js";
 
 const routes = {
   "/": HomeScreen,
   "/product/:id": ProductScreen,
   "/cart/:id": CartScreen,
   "/cart": CartScreen,
+  "/signin":SigninScreen
 };
 
 const router = async () => {
